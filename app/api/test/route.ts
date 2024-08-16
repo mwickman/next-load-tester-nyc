5145141dd7e1dbf1532d855bac3597e47975a648
Matthew Wickman <mattw@section.io>
2024-08-16 12:31:37 -0600

    add options route for CORS

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,39 +1,47 @@
 import { NextRequest, NextResponse } from "next/server";
 const { exec } = require("child_process");
 
-export async function POST(req: NextRequest) {
+export async function OPTIONS(req: Request) {
+  // Handle preflight requests
+  const res = NextResponse.json({ message: "ok" });
+  res.headers.set("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+  res.headers.set("Access-Control-Allow-Headers", "Content-Type");
+  res.headers.set("Access-Control-Allow-Origin", "*");
+  return res;
+}
+
+export async function POST(req: Request, res: Response) {
   const body = await req.json();
   const url = body.url;
   if (!url || url == "") {
     return NextResponse.json({ message: "url is required" }, { status: 400 });
   }
-  const requestCount = body.count
+  const requestCount = body.count;
   if (!requestCount || requestCount <= 0) {
     return NextResponse.json({ message: "count must be a positive number" }, { status: 400 });
   }
-  const proxyURLAndPort = body.proxyURLAndPort
+  const proxyURLAndPort = body.proxyURLAndPort;
   if (!proxyURLAndPort || proxyURLAndPort == "") {
     return NextResponse.json({ message: "proxyURLAndPort is required" }, { status: 400 });
   }
-  const proxyUser = body.proxyUser
+  const proxyUser = body.proxyUser;
   if (!proxyUser || proxyUser == "") {
     return NextResponse.json({ message: "proxyUser is required" }, { status: 400 });
   }
-  const concurrency = body.concurrency
+  const concurrency = body.concurrency;
   if (!concurrency || concurrency <= 0) {
     return NextResponse.json({ message: "concurrency must be a positive number" }, { status: 400 });
   }
 
   // shell out to apachebench and start a load test
-  exec(
-    `ab -n ${requestCount} -c ${concurrency} -X ${proxyURLAndPort} -P ${proxyUser} ${url}`,
-    (err: any, stdout: any, stderr: any) => {
-      if (err) {
-        console.error("Apache bench run failure")
-        console.error(err);
-      }
-      console.log(stdout);
+  exec(`ab -n ${requestCount} -c ${concurrency} -X ${proxyURLAndPort} -P ${proxyUser} ${url}`, (err: any, stdout: any, stderr: any) => {
+    if (err) {
+      console.error("Apache bench run failure");
+      console.error(err);
     }
-  );
-  return NextResponse.json({ message: "started requests" });
+    console.log(stdout);
+  });
+  const finalRes = NextResponse.json({ message: "started requests" });
+  // res.headers.set('Access-Control-Allow-Origin', '*');
+  return finalRes;
 }
